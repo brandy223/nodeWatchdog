1cf80688321eff9b1003719cafc352bb6fd0f456
brandy223 <brandan.legall@gmail.com>
2023-07-07 08:58:51 +0200

    feat: ping ip address

--- src/utils/Network.ts
+++ src/utils/Network.ts
@@ -9,3 +9,16 @@ const ip = require("ip");
 export async function getLocalIP () : Promise<string> {
     return await ip.address();
 }
+
+/**
+ * Ping an IP Address
+ * @param ip
+ * @returns {Promise<boolean>} True if the IP Address is reachable, false otherwise
+ * @throws {Error} If the ip is null or undefined
+ */
+export async function ping (ip: string) : Promise<boolean> {
+    if (ip === undefined || ip === null) throw new Error("IP is null or undefined");
+    const ping = require('ping');
+    const res = await ping.promise.probe(ip);
+    return res.alive;
+}

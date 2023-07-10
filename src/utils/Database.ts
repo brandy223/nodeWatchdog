c0d98d15d1f87bec93fc27a76d622fd6016651d4
brandy223 <brandan.legall@gmail.com>
2023-07-10 13:37:26 +0200

    feat: get the central server fix: get services and servers from jobs take and return an array (instead of one value)

--- src/utils/Database.ts
+++ src/utils/Database.ts
@@ -4,7 +4,7 @@ const prisma = new PrismaClient();
 
 /**
  * Check if a server exists in the database
- * @param ip
+ * @param ip The ip of the server
  * @returns {Promise<boolean>} True if the server exists in the database, false otherwise
  * @throws {Error} If the ip is null or undefined
  */
@@ -16,51 +16,69 @@ export async function isServerInDatabase (ip: string) : Promise<boolean> {
 
 /**
  * Check if a server is a node
- * @param ip
+ * @param ip The ip of the server
  * @returns {Promise<boolean>} True if the server is a node, false otherwise
  * @throws {Error} If the ip is null or undefined
  * @throws {Error} If the server is not in the database
  */
 export async function isServerANode (ip: string) : Promise<boolean> {
     if (ip === undefined || ip === null) throw new Error("IP is null or undefined");
-    if (!await isServerInDatabase(ip)) throw new Error("Server is not in database");
     const server = await prisma.servers.findUnique({ where: { ipAddr: ip } });
-    return server !== undefined && server !== null && server.type === "NODE";
+    if (server === undefined || server === null) throw new Error("Server is not in database");
+    return server.type === "Node";
 }
 
 /**
  * Get server by ip
- * @param ip
+ * @param ip The ip of the server
  * @returns {Promise<*>} The server
  * @throws {Error} If the ip is null or undefined
+ * @throws {Error} If the server is not in the database
  */
 export async function getServerByIP (ip: string) : Promise<any> {
     if (ip === undefined || ip === null) throw new Error("IP is null or undefined");
-    return prisma.servers.findUnique({where: {ipAddr: ip}});
+    const server = prisma.servers.findUnique({where: {ipAddr: ip}});
+    if (server === undefined || server === null) throw new Error("Server is not in database");
+    return server;
+}
+
+/**
+ * Get Central Server
+ * @returns {Promise<*>} The central server
+ * @throws {Error} If the central server is not in the database
+ */
+export async function getCentralServer () : Promise<any> {
+    const centralServer = prisma.servers.findFirst({where: {type: "Central"}});
+    if (centralServer === undefined || centralServer === null) throw new Error("Central server is not in database");
+    return centralServer;
 }
 
 /**
  * Add a server to the database
- * @param ip
+ * @param ip The ip of the server
+ * @param type The type of the server (CENTRAL or NODE)
+ * @param port The port of the server (null if it's a node)
  * @returns {Promise<void>}
  * @throws {Error} If the ip is null or undefined
  * @throws {Error} If the server is already in the database
  */
-export async function addServerToDatabase (ip: string) : Promise<void> {
+export async function addServerToDatabase (ip: string, type: string, port: number | null) : Promise<void> {
     if (ip === undefined || ip === null) throw new Error("IP is null or undefined");
+    if (type === undefined || type === null) throw new Error("Type is null or undefined");
     if (await isServerInDatabase(ip)) throw new Error("Server is already in database");
     await prisma.servers.create({
         data: {
             ipAddr: ip,
-            type: "NODE"
+            type: type,
+            port: port
         }
     });
 }
 
 /**
  * Update server's type
- * @param ip
- * @param type
+ * @param ip The ip of the server
+ * @param type The type of the server (CENTRAL or NODE)
  * @returns {Promise<void>}
  * @throws {Error} If the ip is null or undefined
  * @throws {Error} If the type is null or undefined
@@ -77,24 +95,46 @@ export async function updateServerType (ip: string, type: string) : Promise<void
 }
 
 /**
- * Get the server designated by the job assigned to a node
- * @param job
- * @returns {Promise<*>} A server
- * @throws {Error} If the job is null or undefined
+ * Get servers from jobs
+ * @param jobs[] An array of jobs
+ * @returns {Promise<*>} An array of servers
+ * @throws {Error} If the jobs are null or undefined
+ * @throws {Error} If the server is not in the database
  */
-export async function getServerOfJob (job: any) : Promise<any> {
-    if (job === undefined || job === null) throw new Error("No jobs specified");
+export async function getServersOfJobs (jobs: any[]) : Promise<any> {
+    if (jobs === undefined || jobs === null) throw new Error("No jobs specified");
+    const servers = [];
+    for (const job of jobs) {
+        const server = await prisma.servicesOfServers.findUnique({ where: { jobId: job.id } });
+        const s = await prisma.servers.findUnique({ where: { id: server.serverId } });
+        if (s === undefined || s === null) throw new Error("Server not found");
+        servers.push(s);
+    }
+    return servers;
+}
 
-    const server = await prisma.servicesOfServers.findUnique({ where: { jobId: job.id } });
-    const s = await prisma.servers.findUnique({ where: { id: server.serverId } });
-    if (s === undefined || s === null) throw new Error("Server not found");
-
-    return server;
+/**
+ * Get services from jobs
+ * @param jobs[] An array of jobs
+ * @returns {Promise<*>} An array of services
+ * @throws {Error} If the jobs are null or undefined
+ * @throws {Error} If the service is not in the database
+ */
+export async function getServicesOfJobs (jobs: any[]) : Promise<any> {
+    if (jobs === undefined || jobs === null) throw new Error("No jobs specified");
+    const services = [];
+    for (const job of jobs) {
+        const service = await prisma.servicesOfServers.findUnique({ where: { jobId: job.id } });
+        const s = await getServiceById(service.serviceId);
+        if (s === undefined || s === null) throw new Error("Service not found");
+        services.push(s);
+    }
+    return services;
 }
 
 /**
  * Get a service by id
- * @param id
+ * @param id The id of the service
  * @returns {Promise<*>} The job
  * @throws {Error} If the id is null or undefined
  */
@@ -104,24 +144,8 @@ export async function getServiceById (id: number) : Promise<any> {
 }
 
 /**
- * Get the service designated by the job assigned to a node
- * @param job
- * @returns {Promise<*>} A service
- * @throws {Error} If the job is null or undefined
- */
-export async function getServiceOfJob (job: any) : Promise<any> {
-    if (job === undefined || job === null) throw new Error("No job specified");
-
-    const service = await prisma.servicesOfServers.findUnique({ where: { jobId: job.id } });
-    const s = await prisma.services.findUnique({ where: { id: service.serviceId } });
-    if (s === undefined || s === null) throw new Error("Service not found");
-
-    return service;
-}
-
-/**
- * Get all jobs assigned to a node
- * @param ip
+ * Get all jobs assigned to a server
+ * @param ip The ip of the server
  * @returns {Promise<*>} An array of jobs
  * @throws {Error} If the ip is null or undefined
  * @throws {Error} If the server is not in the database

6957a3a4457ff62d8f44d7299cc8711151498ae5
brandy <brandan.le-gall@isen.yncrea.fr>
2023-07-11 17:52:31 +0200

    fix: adapted socket io client declaration to socket io package version

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,58 @@ const Timer = require('./utils/Timer')
  * Main function
  */
 async function main (): Promise<void> {
+    await nodeServerDatabaseInit();
+    const centralServer = await getCentralServer();
 
+    const socket = io(`http://${centralServer.ipAddr}:${centralServer.port}`, {
+        reconnection: true,
+        // cors: {
+        //   credentials: true,
+        //   origin: 'http://192.168.10.44',
+        //   methods: ['GET', 'POST']
+        // },
+        withCredentials: true,
+        transports: ["polling"],
+        allowEIO3: true, // false by default
+    });
+
+    socket.on("error", function () {
+        console.log("Sorry, there seems to be an issue with the connection!");
+    });
+
+    socket.on("connect_error", function (err: Error) {
+        console.error("connect failed " + err);
+    });
+
+    socket.on('connect', () => {
+        console.log(`Connected to central server`);
+    });
+
+    // // GET ALL JOBS
+    // const jobs = await Database.getAllJobsOfNode(ip);
+    // if (jobs.length === 0) throw new Error("No jobs found");
+    // else
+    //     console.log(`Jobs: ${JSON.stringify(jobs)}`);
+    //
+    // // GET ALL SERVERS IP
+    // const servers = await Database.getServersOfJobs(jobs);
+    // if (servers.length === 0) throw new Error("No servers found");
+    // console.log(`Servers: ${JSON.stringify(servers)}`);
+    //
+    // // Ping servers to check if they are alive
+    // const aliveServers = await Network.pingServers(servers.map(( server: any ) => server.ipAddr));
+    // if (aliveServers.length === 0) throw new Error("No alive servers found");
+    // console.log(`Alive servers: ${JSON.stringify(aliveServers)}`);
+    //
+    // // GET ALL SERVICES OF ALIVE SERVERS
+    // const services = await Database.getServicesOfJobs(jobs);
+    // if (services.length === 0) throw new Error("No services found");
+    // console.log(`Services: ${JSON.stringify(services)}`);
+}
+
+main()
+
+async function nodeServerDatabaseInit(): Promise<void> {
     // GET LOCAL IP
     const ip = await Network.getLocalIP();
     if (ip === undefined)  throw new Error("Could not get local IP");
@@ -17,56 +68,37 @@ async function main (): Promise<void> {
         console.log(`Local IP: ${ip}`);
 
     // VERIFY NODE EXISTS IN DATABASE
-    const isServerInDatabase = await Database.isServerInDatabase(ip);
-    if (!isServerInDatabase) {
-        await Database.addServerToDatabase(ip, "Node", null);
+    if (!await Database.isServerInDatabase(ip)) {
+        await Database.addServerToDatabase(ip, "Node", null, null);
         console.log(`Added node server to database`);
     }
     else {
         const isServerANode = await Database.isServerANode(ip);
         if(!isServerANode) {
-            await Database.updateServerType(ip, "Node");
+            await Database.updateServer(ip, "Node", null, null);
         }
     }
-
-    // GET CENTRAL SERVER IP
-    const centralServer = await Database.getCentralServer();
-    if (centralServer === undefined) throw new Error("Central server not found");
-    else
-        console.log(`Central server: ${JSON.stringify(centralServer)}`);
-
-    // PING CENTRAL SERVER
-    const isCentralServerAlive = await Network.pingServer(centralServer.ipAddr);
-    if (!isCentralServerAlive) throw new Error("Central server is not alive");
-    else
-        console.log(`Central server is alive`);
-
-    // CONNECT TO CENTRAL SERVER
-    const socket = io(`http://${centralServer.ipAddr}:3000`);
-    socket.on('connect', () => {
-        console.log(`Connected to central server`);
-    });
-
-    // GET ALL JOBS
-    const jobs = await Database.getAllJobsOfNode(ip);
-    if (jobs.length === 0) throw new Error("No jobs found");
-    else
-        console.log(`Jobs: ${JSON.stringify(jobs)}`);
-
-    // GET ALL SERVERS IP
-    const servers = await Database.getServersOfJobs(jobs);
-    if (servers.length === 0) throw new Error("No servers found");
-    console.log(`Servers: ${JSON.stringify(servers)}`);
-
-    // Ping servers to check if they are alive
-    const aliveServers = await Network.pingServers(servers.map(( server: any ) => server.ipAddr));
-    if (aliveServers.length === 0) throw new Error("No alive servers found");
-    console.log(`Alive servers: ${JSON.stringify(aliveServers)}`);
-
-    // GET ALL SERVICES OF ALIVE SERVERS
-    const services = await Database.getServicesOfJobs(jobs);
-    if (services.length === 0) throw new Error("No services found");
-    console.log(`Services: ${JSON.stringify(services)}`);
 }
 
-main()
+async function getCentralServer(): Promise<any> {
+    // GET CENTRAL SERVER IP
+    const centralServer = await Database.getServerByType("Central");
+    if (centralServer === undefined || centralServer === null) throw new Error("Central server not found");
+    const mainServer = centralServer.filter((server: any) => server.priority === 1)
+    console.log(`Central server: ${JSON.stringify(mainServer)}`);
+
+    // PING CENTRAL SERVER
+    const isCentralServerAlive = await Network.ping(mainServer[0].ipAddr);
+    if (isCentralServerAlive) {
+        console.log(`Central server is alive`);
+        return mainServer[0];
+    }
+
+    console.log(`Central server is not alive, trying to connect to backup server`);
+    const backupServer = centralServer.filter((server: any) => server.priority === 0)
+    const isBackupServerAlive = await Network.pingServers([backupServer[0].ipAddr]);
+    if (!isBackupServerAlive) throw new Error("Backup central server is not alive");
+    console.log(`Backup server: ${JSON.stringify(backupServer)}`);
+
+    return backupServer[0];
+}

878541d1ebbcd99fefd714eef256aa5719a04f3a
brandy <brandan.le-gall@isen.yncrea.fr>
2023-07-13 16:40:20 +0200

    feat: isServiceActive now returns status boolean + text fix: stdout wasn't await, so isActive was always false

--- src/services/BasicServices.ts
+++ src/services/BasicServices.ts
@@ -1,27 +1,47 @@
+import { promisify } from "util";
 
 const { spawn } = require("node:child_process");
+
+const theme = require("../utils/ColorScheme").theme;
 
 /**
  * Tell state of a service through systemctl
  * @param {any} server Server object that contains the IP address and the user to connect to through SSH
  * @param {any} service The service object
- * @return {Promise<boolean>} True if the service is active, false otherwise
+ * @return {Promise<string[]>} Contains the state of the service and the output of the command on "Active: " line
  * @throws {Error} If the server is null or undefined
  * @throws {Error} If the service is null or undefined
  * @throws {Error} If the data from ssh stdout is null or undefined
  */
-export async function isServiceActive (server: any, service: any) : Promise<boolean> {
+export async function isServiceActive(server: any, service: any): Promise<string[]> {
     if (server === undefined || server === null) throw new Error("Server is null or undefined");
     if (service === undefined || service === null) throw new Error("Service is null or undefined");
-    let isActive: boolean = false;
 
-    const arg1 = `${server.user}@${server.ipAddr}`;
-    const arg2 = `systemctl is-active ${service.name}.service`;
-    const conn = spawn("ssh", [arg1, arg2]);
-    conn.stdout.on("data", (data: any): void => {
-        if (data === undefined || data === null) throw new Error("Data is null or undefined");
-        if(data.toString().trim() === "active")
-            isActive = true;
+    return new Promise<string[]>((resolve, reject): void => {
+        const arg1 = `${server.user}@${server.ipAddr}`;
+        const arg2 = `systemctl status ${service.name}.service`;
+        const conn = spawn("ssh", [arg1, arg2]);
+
+        const output: string[] = [];
+
+        conn.stdout.on("data", (data: any): void => {
+            const line: string = data.toString().trim().split("\n")[2].split("Active:").map((s: string) => s.trim())[1];
+            if (line.includes("active")) output.push("true");
+            else if (line.includes("inactive")) output.push("false");
+            else output.push("unknown");
+            output.push(line);
+        });
+
+        conn.stderr.on("data", (err: any): void => {
+            reject(new Error(err.toString()));
+        });
+
+        conn.on("close", (code: number): void => {
+            if (code === 0) {
+                resolve(output);
+            } else {
+                reject(new Error(`Process (from isServiceActive()) exited with code ${code}`));
+            }
+        });
     });
-    return isActive;
 }

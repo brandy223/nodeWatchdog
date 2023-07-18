20042d2dcef45f293b8c047fae41a36aa1c5c94d
brandy <brandan.le-gall@isen.yncrea.fr>
2023-07-18 16:08:10 +0200

    refactor: moved functions to central server

--- src/utils/Message.ts
+++ src/utils/Message.ts
@@ -6,45 +6,6 @@ const io= require('socket.io-client');
 const theme = require('./ColorScheme').theme;
 
 /**
- * Verify if someone is free to receive a message
- * @param {number} id The id of the person
- * @returns {Promise<boolean>} True if the person is free, false otherwise
- * @throws {Error} If the id is null or undefined
- * @throws {Error} If the person is not in the database
- */
-export async function isPersonFree (id: number) : Promise<boolean> {
-    return false;
-}
-
-/**
- * Send a message to someone
- * @param {string} number The number to send the message to
- * @param {string} message The message to send
- * @returns {Promise<void>}
- * @throws {Error} If the number is null or undefined
- * @throws {Error} If the message is null or undefined
- * @throws {Error} If the number is not valid
- * @throws {Error} If the number is not reachable
- */
-export async function sendMessage (number: string, message: string) : Promise<void> {
-
-}
-
-/**
- * Email someone
- * @param {string} email The email to send the message to
- * @param {string} message The message to send
- * @returns {Promise<void>}
- * @throws {Error} If the email is null or undefined
- * @throws {Error} If the message is null or undefined
- * @throws {Error} If the email is not valid
- * @throws {Error} If the email is not reachable
- */
-export async function sendEmail (email: string, message: string) : Promise<void> {
-
-}
-
-/**
  * Send JSON data to main server
  * @param {any} data The data to send
  * @returns {Promise<void>}
